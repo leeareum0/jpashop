5e127774e71f2a591b22266c78498e6b9308b7e0
이아름 <areum6321@gmail.com>
2024-01-10 15:20:47 +0900

    엔티티 클래스 - 배송 엔티티 코드

---
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,27 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+public class Delivery {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "delivery_id")
+    private Long id;
+
+    @OneToOne(mappedBy = "delivery", fetch = FetchType.LAZY)
+    private Order order;
+
+    @Embedded
+    private Address address;
+
+    @Enumerated(EnumType.STRING) //EnumType.ORDINAL - 중간에 다른 상태가 생기면 오류나기 때문에 String으로 꼭 사용해야함
+    private DeliveryStatus status; //READY, COMP
+
+
+
+}
