5d58603190d1ae9dcb939bd816b031ef1df5c77f
이아름 <areum6321@gmail.com>
2024-01-10 15:21:34 +0900

    엔티티 클래스 - 주소 값 타입 코드

---
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -0,0 +1,24 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.Embeddable;
+import lombok.Getter;
+import lombok.Setter;
+
+@Embeddable
+@Getter
+public class Address { //값 타입
+
+    private String city;
+    private String street;
+    private String zipcode;
+
+    protected Address() { //jpa 스펙상 설정
+
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
+}
