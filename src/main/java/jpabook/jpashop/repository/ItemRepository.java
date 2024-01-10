11c7019c6a74b9574dbe9edb459b8da316403504
이아름 <areum6321@gmail.com>
2024-01-10 16:00:56 +0900

    상품 도메인 개발 - 상품 리포지토리 코드

---
+++ src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,32 @@
+package jpabook.jpashop.repository;
+
+import jakarta.persistence.EntityManager;
+import jpabook.jpashop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    private final EntityManager em;
+
+    //item 값이 없을 때 신규 생성되는 코드(업데이트)
+    public void save(Item item) {
+        if (item.getId() == null) {
+            em.persist(item);
+        }else {
+            em.merge(item);
+        }
+    }
+
+    public Item findOne(Long id) {
+        return em.find(Item.class, id);
+    }
+
+    public List<Item> findAll() {
+        return em.createQuery("select i from Item i", Item.class).getResultList();
+    }
+}
