1583329bb1e69faa06aee689e0824a1cc0c90936
이아름 <areum6321@gmail.com>
2024-01-10 15:24:22 +0900

    회원 도메인 개발 - 회원 서비스 코드

---
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,46 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+  /*
+    @Transactional(readOnly = true)
+    읽기 전용에 (readOnly = true) 사용
+    - 조회 시 jpa 성능을 최적화한다.
+    */
+@Transactional(readOnly = true)
+@RequiredArgsConstructor //final이 있는 필드를 가지고 생성자를 생성해주는 어노테이션
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    //회원 가입
+    @Transactional
+    public Long join(Member member) {
+        validateDuplicateMember(member); //중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    //회원 전체 조회
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+}
