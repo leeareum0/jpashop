6e14473528f47c4dcb264f122c19101524924492
이아름 <areum6321@gmail.com>
2024-03-05 18:04:21 +0900

    API 개발 컬렉션 조회 최적화 - 주문 조회 V4: JPA에서 DTO 직접 조회  OrderItemQueryDto 추가

---
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderItemQueryDto.java
@@ -0,0 +1,22 @@
+package jpabook.jpashop.repository.order.query;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+
+@Data
+public class OrderItemQueryDto {
+
+    //엔티티 x , 화면에서 확인 하기 위해 만든 Dto
+    @JsonIgnore
+    private Long orderId;
+    private String itemName;
+    private int orderPrice;
+    private int count;
+
+    public OrderItemQueryDto(Long orderId, String itemName, int orderPrice, int count) {
+        this.orderId = orderId;
+        this.itemName = itemName;
+        this.orderPrice = orderPrice;
+        this.count = count;
+    }
+}
